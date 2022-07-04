575c195d315eaf5d691550719827db9850eb386d
a001 <example@gmail.com>
2022-07-04 22:28:26 +0800

    add MysqlConfig.java

---
+++ spring-server/src/main/java/ntcu/selab/SpringServer/config/MysqlConfig.java
@@ -0,0 +1,12 @@
+package ntcu.selab.SpringServer.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MysqlConfig {   
+    private static final String PROPERTY_FILE = "db_config.properties";
+}
