68466929a048e62342a6764770e0ac2ac86f33ee
jh08666 <acs108118@gm.ntcu.edu.tw>
2022-07-07 22:00:37 +0800

    modify property_file

--- spring-server/src/main/java/ntcu/selab/SpringServer/config/MysqlConfig.java
+++ spring-server/src/main/java/ntcu/selab/SpringServer/config/MysqlConfig.java
@@ -6,82 +6,82 @@ import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MysqlConfig {   
-    private static final String PROPERTY_FILE = "mysql.properties";
+public class MysqlConfig {
+    private static final String PROPERTY_FILE = "/mysql.properties";
     private static MysqlConfig object = new MysqlConfig();
     private static final Logger logger = LoggerFactory.getLogger(MysqlConfig.class);
     private Properties props;
-    
-    public MysqlConfig(){
+
+    public MysqlConfig() {
         InputStream is = this.getClass().getResourceAsStream(PROPERTY_FILE);
-        try{
+        try {
             props = new Properties();
             props.load(is);
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error(e.getMessage());
         }
     }
 
-    public static MysqlConfig getObject(){
+    public static MysqlConfig getObject() {
         return object;
     }
 
-    public String getDBUser()throws Exception{
+    public String getDBUser() throws Exception {
         String dbuser = System.getenv("DB_USER");
-        if(dbuser != null && !dbuser.equals("")){
+        if (dbuser != null && !dbuser.equals("")) {
             return dbuser;
         }
-        if(props != null){
+        if (props != null) {
             return props.getProperty("DB_USER").trim();
         }
         throw new Exception("Could not found DB_USER");
     }
 
-    public String getDBPassword() throws Exception{
+    public String getDBPassword() throws Exception {
         String dbpassword = System.getenv("DB_PASSWORD");
-        if(dbpassword!= null && !dbpassword.equals("")){
+        if (dbpassword != null && !dbpassword.equals("")) {
             return dbpassword;
         }
-        if(props != null){
+        if (props != null) {
             return props.getProperty("DB_PASSWORD").trim();
         }
         throw new Exception("Could not found DB_PASSWORD");
     }
 
-    public String getDBSchema() throws Exception{
+    public String getDBSchema() throws Exception {
         String dbschema = System.getenv("DB_DATABASE");
-        if(dbschema!= null && !dbschema.equals("")){
+        if (dbschema != null && !dbschema.equals("")) {
             return dbschema;
         }
-        if(props != null){
+        if (props != null) {
             return props.getProperty("DB_DATABASE").trim();
         }
-        throw new Exception("Could not found DB_DATABASE"); 
+        throw new Exception("Could not found DB_DATABASE");
     }
 
-    public String getDBHost() throws Exception{
+    public String getDBHost() throws Exception {
         String dbhost = System.getenv("DB_HOST");
-        if(dbhost!= null && !dbhost.equals("")){
+        if (dbhost != null && !dbhost.equals("")) {
             return dbhost;
         }
-        if(props != null){
+        if (props != null) {
             return props.getProperty("DB_HOST").trim();
         }
-        throw new Exception("Could not found DB_HOST"); 
+        throw new Exception("Could not found DB_HOST");
     }
 
-    public String getDBConnectionOpt() throws Exception{
+    public String getDBConnectionOpt() throws Exception {
         String dbconnopt = System.getenv("DB_CONNECTION_OPTION");
-        if(dbconnopt!= null && !dbconnopt.equals("")){
+        if (dbconnopt != null && !dbconnopt.equals("")) {
             return dbconnopt;
         }
-        if(props != null){
+        if (props != null) {
             return props.getProperty("DB_CONNECTION_OPTION").trim();
         }
-        throw new Exception("Could not found DB_CONNECTION_OPTION"); 
+        throw new Exception("Could not found DB_CONNECTION_OPTION");
     }
 
-    public String getConnectionString() throws Exception{
-        return "jdbc:mysql://" + getDBHost() + "/" + getDBSchema() + getDBConnectionOpt(); 
+    public String getConnectionString() throws Exception {
+        return "jdbc:mysql://" + getDBHost() + "/" + getDBSchema() + getDBConnectionOpt();
     }
 }
