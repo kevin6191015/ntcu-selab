d37da3996ae42046b0615b9c2341ffbb8d8fd5de
jh08666 <acs108118@gm.ntcu.edu.tw>
2022-07-09 21:02:41 +0800

    add Question.java

---
+++ spring-server/src/main/java/ntcu/selab/SpringServer/data/Question.java
@@ -0,0 +1,247 @@
+package ntcu.selab.SpringServer.data;
+
+public class Question {
+    private int id = 0;
+    private String name = null;
+    private String description = null;
+    private String image1 = null;
+    private String image2 = null;
+    private String input1 = null;
+    private String input2 = null;
+    private String input3 = null;
+    private String input4 = null;
+    private String input5 = null;
+    private String input6 = null;
+    private String input7 = null;
+    private String input8 = null;
+    private String input9 = null;
+    private String input10 = null;
+    private String output1 = null;
+    private String output2 = null;
+    private String output3 = null;
+    private String output4 = null;
+    private String output5 = null;
+    private String output6 = null;
+    private String output7 = null;
+    private String output8 = null;
+    private String output9 = null;
+    private String output10 = null;
+    private int inputornot = 0;
+
+    public Question(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public Question(){
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getImage1(){
+        return image1;
+    }
+
+    public void setImage1(String image1){
+        this.image1 = image1;
+    }
+
+    public String getImage2(){
+        return image2;
+    }
+
+    public void setImage2(String image2){
+        this.image2 = image2;
+    }
+
+    public String getInput1(){
+        return input1;
+    }
+
+    public void setInput1(String input1){
+        this.input1 = input1;
+    }
+
+    public String getInput2(){
+        return input2;
+    }
+
+    public void setInput2(String input2){
+        this.input2 = input2;
+    }
+
+    public String getInput3(){
+        return input3;
+    }
+
+    public void setInput3(String input3){
+        this.input3 = input3;
+    }
+
+    public String getInput4(){
+        return input4;
+    }
+
+    public void setInput4(String input4){
+        this.input4 = input4;
+    }
+
+    public String getInput5(){
+        return input5;
+    }
+
+    public void setInput5(String input5){
+        this.input5 = input5;
+    }
+
+    public String getInput6(){
+        return input6;
+    }
+
+    public void setInput6(String input6){
+        this.input6 = input6;
+    }
+
+    public String getInput7(){
+        return input7;
+    }
+
+    public void setInput7(String input7){
+        this.input7 = input7;
+    }
+
+    public String getInput8(){
+        return input8;
+    }
+
+    public void setInput8(String input8){
+        this.input8 = input8;
+    }
+
+    public String getInput9(){
+        return input9;
+    }
+
+    public void setInput9(String input9){
+        this.input9 = input9;
+    }
+
+    public String getInput10(){
+        return input10;
+    }
+
+    public void setInput10(String input10){
+        this.input10 = input10;
+    }
+
+    public String getOutput1(){
+        return output1;
+    }
+
+    public void setOutput1(String output1){
+        this.output1 = output1;
+    }
+
+    public String getOutput2(){
+        return output2;
+    }
+
+    public void setOutput2(String output2){
+        this.output2 = output2;
+    }
+
+    public String getOutput3(){
+        return output3;
+    }
+
+    public void setOutput3(String output3){
+        this.output3 = output3;
+    }
+
+    public String getOutput4(){
+        return output4;
+    }
+
+    public void setOutput4(String output4){
+        this.output4 = output4;
+    }
+
+    public String getOutput5(){
+        return output5;
+    }
+
+    public void setOutput5(String output5){
+        this.output5 = output5;
+    }
+
+    public String getOutput6(){
+        return output6;
+    }
+
+    public void setOutput6(String output6){
+        this.output6 = output6;
+    }
+
+    public String getOutput7(){
+        return output7;
+    }
+
+    public void setOutput7(String output7){
+        this.output7 = output7;
+    }
+
+    public String getOutput8(){
+        return output8;
+    }
+
+    public void setOutput8(String output8){
+        this.output8 = output8;
+    }
+
+    public String getOutput9(){
+        return output9;
+    }
+
+    public void setOutput9(String output9){
+        this.output9 = output9;
+    }
+
+    public String getOutput10(){
+        return output10;
+    }
+
+    public void setOutput10(String output10){
+        this.output10 = output10;
+    }
+
+    public int getInputornot(){
+        return inputornot;
+    }
+
+    public void setInputornot(int inputornot){
+        this.inputornot = inputornot;
+    }
+}
