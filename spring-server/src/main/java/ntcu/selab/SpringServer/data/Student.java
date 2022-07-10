426c0ecb9b5f722d738a90a36c55e48bd68ccf34
jh08666 <acs108118@gm.ntcu.edu.tw>
2022-07-10 21:41:12 +0800

    add StudentDBManager

---
+++ spring-server/src/main/java/ntcu/selab/SpringServer/data/Student.java
@@ -0,0 +1,31 @@
+package ntcu.selab.SpringServer.data;
+
+public class Student {
+    private String id = null;
+    private String name = null;
+
+    public Student(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Student(){
+
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
