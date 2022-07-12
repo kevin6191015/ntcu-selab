e063aba99556918f9b997027d257bc757d635463
jh08666 <acs108118@gm.ntcu.edu.tw>
2022-07-12 17:21:01 +0800

    add CourseService

---
+++ spring-server/src/main/java/ntcu/selab/SpringServer/service/CourseService.java
@@ -0,0 +1,99 @@
+package ntcu.selab.SpringServer.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ntcu.selab.SpringServer.data.Course;
+import ntcu.selab.SpringServer.db.CourseDBManager;
+
+@RestController
+@RequestMapping(value = "/course")
+public class CourseService {
+    public static CourseService cs = new CourseService();
+    private static final Logger logger = LoggerFactory.getLogger(StudentService.class);
+    private static CourseDBManager cDbManager = CourseDBManager.getObject();
+
+    public static CourseService getObject(){
+        return cs;
+    }
+
+    @GetMapping("getCourses")
+    public ResponseEntity<Object> getCourses() throws Exception{
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content-Type", "application/json");
+
+        List<Course> courses = cDbManager.getCourses();
+        List<JSONObject> courseList = new ArrayList<>();
+        try{
+            for(Course course : courses){
+                JSONObject object = new JSONObject();
+                object.put("class_name", course.getCourseName());
+                object.put("teacher", course.getTA());
+                object.put("TA", course.getTA());
+                courseList.add(object);
+            }
+        }catch(Exception e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>("Failed!", header, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        JSONObject root = new JSONObject();
+        root.put("Courses", courseList);
+        return new ResponseEntity<Object>(root, header, HttpStatus.OK);
+    }
+
+    @GetMapping("addCourse")
+    public ResponseEntity<Object> addCourse(@RequestParam String class_name, @RequestParam String teacher, @RequestParam String TA){
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content_Type", "application/json");
+
+        try{
+            Course course = new Course(class_name, teacher, TA);
+            cDbManager.addCourse(course);
+        }catch(Exception e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>("Failed!", header, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(header, HttpStatus.OK);
+    }
+
+    @GetMapping("updateCourse")
+    public ResponseEntity<Object> updateCourse(@RequestParam String class_id, @RequestParam String class_name
+    , @RequestParam String teacher, @RequestParam String TA){
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content_Type", "application/json");
+
+        try{
+            Course course = new Course(class_name, teacher, TA);
+            cDbManager.updateCourse(class_id, course);
+        }catch(Exception e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>("Failed!", header, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(header, HttpStatus.OK);
+    }
+
+    @GetMapping("deleteCourse")
+    public ResponseEntity<Object> deleteCourse(@RequestParam String class_id){
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content_Type", "application/json");
+        JSONObject jsonObject = null;
+        try{
+            jsonObject = cDbManager.deleteCourse(class_id);
+            header.add("Custom-Header,", "message");
+        }catch(Exception e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>("Failed!", header, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(jsonObject, header, HttpStatus.OK); 
+    }
+}
