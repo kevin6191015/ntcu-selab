031782249a4b776ee54c357a0bea66ae4aa46a93
jh08666 <acs108118@gm.ntcu.edu.tw>
2022-07-13 16:34:23 +0800

    unuse

--- spring-server/src/main/java/ntcu/selab/SpringServer/db/service/UserDBService.java
+++ spring-server/src/main/java/ntcu/selab/SpringServer/db/service/UserDBService.java
@@ -1,27 +1,28 @@
-package ntcu.selab.SpringServer.db.service;
 
-import ntcu.selab.SpringServer.data.User;
-import ntcu.selab.SpringServer.db.UserDBManager;
-import java.util.ArrayList;
-import java.util.List;
+// package ntcu.selab.SpringServer.db.service;
 
-public class UserDBService {
-    private static UserDBService object = new UserDBService();
-    private UserDBManager udb = UserDBManager.getObject();
+// import ntcu.selab.SpringServer.data.User;
+// import ntcu.selab.SpringServer.db.UserDBManager;
+// import java.util.ArrayList;
+// import java.util.List;
 
-    public static UserDBService getObject(){
-        return object;
-    }
+// public class UserDBService {
+//     private static UserDBService object = new UserDBService();
+//     private UserDBManager udb = UserDBManager.getObject();
 
-    public int getGitlabId(String username){
-        return udb.getGitlabidByUsername(username);
-    }
+//     public static UserDBService getObject(){
+//         return object;
+//     }
 
-    public int getId(String username){
-        return udb.getUseridByUsername(username);
-    }
+//     public int getGitlabId(String username){
+//         return udb.getGitlabidByUsername(username);
+//     }
 
-    public String getName(int id){
-        return udb.getUsernameById(id);
-    }
-}
+//     public int getId(String username){
+//         return udb.getUseridByUsername(username);
+//     }
+
+//     public String getName(int id){
+//         return udb.getUsernameById(id);
+//     }
+// }
