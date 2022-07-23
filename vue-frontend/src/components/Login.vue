391c6859498e2d081d8b5f2265fa9e29f7e3c503
kevin <kevin6191015@gmail.com>
2022-07-23 18:04:23 +0800

    webpack with vue

---
+++ vue-frontend/src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class= "login" >
+      <h3> 登錄 </h3>
+      用戶名 : <input type="text" v-model="loginForm.loginName" placeholder="請輸入用户名"/>
+      <br><br>
+      密碼 ：  <input type="password" v-model="loginForm.password" placeholder="請輸入密碼"/>
+      <br><br>
+      <button> 登錄</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      loginForm: {
+        loginName: '',
+        password: ''
+      },
+      responseResult: []
+    }
+  },
+  methods: {
+  }
+}
+</script>
