a164594b0db70e5f7d230487f4e590b27846625f
jh08666 <acs108118@gm.ntcu.edu.tw>
2022-08-05 02:04:03 +0800

    fix timestamp validation

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -17,20 +17,23 @@ axios.default.baseURL = '/data'
 router.beforeEach((to, from, next) => {
   // 路由需要認證
   if (to.meta.requireAuth) {
-    // 取得token中的到期時間
-    var base64Url = store.state.token.split('.')[1]
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-    }).join(''))
-    var Expiretime = JSON.parse(jsonPayload).exp
-    // 取得當前時間
-    var now = Math.floor(new Date().getTime() / 1000)
-    // 憑證有效時長
-    var Validatetime = 30 * 60 * 1000
-    // 判斷store裡是否有token或是token已過期
-    if (store.state.token && now - Expiretime < Validatetime) {
-      next()
+    // 判斷store裡是否有token
+    if (store.state.token) {
+      // 取得token中的到期時間
+      var base64Url = store.state.token.split('.')[1]
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+      var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+      }).join(''))
+      var Expiretime = JSON.parse(jsonPayload).exp
+      // 取得當前時間
+      var now = Math.floor(new Date().getTime() / 1000)
+      // 憑證有效時長
+      var Validatetime = 30 * 60 * 1000
+      // 判斷token是否已過期
+      if (now - Expiretime < Validatetime) {
+        next()
+      }
     } else {
       next({
         path: '/login',
