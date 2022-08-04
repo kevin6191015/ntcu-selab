82c25817f35509fd7aa0f10aa1d865200281ec17
jh08666 <acs108118@gm.ntcu.edu.tw>
2022-08-05 01:53:51 +0800

    add timestamp validation

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -17,8 +17,19 @@ axios.default.baseURL = '/data'
 router.beforeEach((to, from, next) => {
   // 路由需要認證
   if (to.meta.requireAuth) {
-    // 判斷store裡是否有token
-    if (store.state.token) {
+    // 取得token中的到期時間
+    var base64Url = store.state.token.split('.')[1]
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+    }).join(''))
+    var Expiretime = JSON.parse(jsonPayload).exp
+    // 取得當前時間
+    var now = Math.floor(new Date().getTime() / 1000)
+    // 憑證有效時長
+    var Validatetime = 30 * 60 * 1000
+    // 判斷store裡是否有token或是token已過期
+    if (store.state.token && now - Expiretime < Validatetime) {
       next()
     } else {
       next({
