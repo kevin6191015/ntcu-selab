f4d681b5658ae8974dd19ab3d711bd1f44b2ec18
jh08666 <acs108118@gm.ntcu.edu.tw>
2022-09-08 16:34:07 +0800

    implement ShowCourseStudent&ShowHomework and fix bug

---
+++ vue-frontend/src/components/ShowCourseStudent.vue
@@ -0,0 +1,140 @@
+<template>
+  <el-container class="container2">
+    <el-main>
+      <el-row>
+        <el-col class="title1">{{this.question_name}}</el-col>
+        <el-col class="title2">曲線圖</el-col>
+      </el-row>
+      <div style="margin:10px"></div>
+      <el-row>
+        <el-table
+          :data="tableData"
+          stripe
+          highlight-current-row
+          @current-change="seleted"
+          style="width: 100%">
+          <el-table-column
+            prop="student_id"
+            label="學生姓名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="submit_times"
+            label="作答次數"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="unit_tes_score"
+            label="作答分數">
+          </el-table-column>
+          <el-table-column
+            prop="analysis_date"
+            label="繳交時間">
+          </el-table-column>
+        </el-table>
+      </el-row>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import { getLatestScore } from '@/api/score'
+import { getStudent } from '../api/student'
+import store from '../store'
+export default {
+  name: 'ShowCourseStudent',
+  data () {
+    return {
+      question_name: store.state.assignment.question_name,
+      tableData: [],
+      student: [],
+      project_name: store.state.assignment.question_id + '_' + store.state.class.substring(0, 5) + '_' + store.state.assignment.release_time
+    }
+  },
+  created () {
+    getStudent({
+      class_id: store.state.class_id
+    }).then(res => {
+      this.student = res.data.data.Students
+    })
+    getLatestScore({
+      project_name: this.project_name
+    }).then(res => {
+      let tmp = res.data.data.Scores
+      for (let i = 0; i < tmp.length; i++) {
+        for (let j = 0; j < this.student.length; j++) {
+          if (tmp[i].student_id === this.student[j].student_id) {
+            tmp[i].student_id = this.student[j].student_name
+            break
+          }
+        }
+      }
+      this.tableData = tmp
+    })
+  },
+  methods: {
+    seleted (val) {
+      console.log(val)
+    }
+  }
+}
+</script>
+
+<style>
+.container2 {
+  width: 1250px;
+  margin: 0 auto;
+  background-color: rgba(111, 122, 144, 0.555);
+  height: 650px;
+}
+
+.d-flex1 {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.el-col1 {
+  border-radius: 10px;
+  min-height: 36px;
+  margin-left: 13%;
+}
+
+.grid-content1 {
+  min-height: 36px;
+  font-size: 30px;
+  background-color: orange;
+  padding-left: 30%;
+}
+
+.grid-content2 {
+  min-height: 36px;
+  font-size: 30px;
+  margin-left: 28.4%;
+  width: 72.5%;
+}
+
+.grid-content3 {
+  font-size: 13px;
+  padding-left: 50%
+}
+
+.title1 {
+  width: auto;
+  float:left;
+  background-color:yellow;
+  font-size: 20px;
+  padding: 10px;
+  border: 4px solid rgba(0, 0, 0, 0.397);
+  border-radius: 12px
+}
+
+.title2 {
+  width: auto;
+  float:right;
+  background-color:yellow;
+  font-size: 20px;
+  padding: 10px;
+  border: 4px solid rgba(0, 0, 0, 0.397);
+  border-radius: 12px
+}
+</style>
