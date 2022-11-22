9d97f32e66837d79b0d0fad76aa90e42665a95b2
acs108102-ntcu <acs108102@gm.ntcu.edu.tw>
2022-11-22 22:15:51 +0800

    Instructor Commit

---
+++ spring-server/src/main/resources/maven/a0008_001_108-1_20221122_acs109211/src/main/java/edu/selab/App.java
@@ -0,0 +1,14 @@
+package edu.selab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String main( String args )
+    {
+        System.out.println( "Hello World!" );
+        return "Hello World!";
+    }
+}
